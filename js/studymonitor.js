ccaf35e834bb304df209ed656a96f912be2c005c
Robin-Smits <smit0141@hz.nl>
2022-10-25 12:54:35 +0200

    createTable

--- js/studymonitor.js
+++ js/studymonitor.js
@@ -200,121 +200,115 @@ const schoolYearOne = [
 ]
 console.log(schoolYearOne);
 
-function makeProgressBarRed(){
+function makeProgressBarRed() {
     laadbalk = document.getElementById("laadbalk")
     laadbalk.classList.add("laadbalkUnderNBSA");
 }
-function makeProgressBarYellow(){
+function makeProgressBarYellow() {
     laadbalk = document.getElementById("laadbalk")
     laadbalk.classList.add("laadbalkJustAboveNBSA");
 }
-function makeProgressBarBlue(){
+function makeProgressBarBlue() {
     laadbalk = document.getElementById("laadbalk")
     laadbalk.classList.add("laadbalkSaveForNBSA");
 }
 
-function calculateMyEc (array){
-for (i = 0; i < schoolYearOne.length; i++) {
-    for (j = 0; j < schoolYearOne[i].kwartaal.length; j++) {
-        for (k = 0; k < schoolYearOne[i].kwartaal[j].course.length; k++) {
-            for (l = 0; l < schoolYearOne[i].kwartaal[j].course[k].exam.length; l++) {
-            totalEC += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
-            console.log(`your Course= ${schoolYearOne[i].kwartaal[j].course[k].name}, your name= ${schoolYearOne[i].kwartaal[j].course[k].exam[l].name}& your weight =${schoolYearOne[i].kwartaal[j].course[k].exam[l].weight} & `);
-            if (schoolYearOne[i].kwartaal[j].course[k].exam[l].grade >= 5.5) {
-                myTotalEC += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
+function calculateMyEc(array) {
+    for (i = 0; i < schoolYearOne.length; i++) {
+        for (j = 0; j < schoolYearOne[i].kwartaal.length; j++) {
+            for (k = 0; k < schoolYearOne[i].kwartaal[j].course.length; k++) {
+                for (l = 0; l < schoolYearOne[i].kwartaal[j].course[k].exam.length; l++) {
+                    totalEC += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
+                    console.log(`your Course= ${schoolYearOne[i].kwartaal[j].course[k].name}, your name= ${schoolYearOne[i].kwartaal[j].course[k].exam[l].name}& your weight =${schoolYearOne[i].kwartaal[j].course[k].exam[l].weight} & `);
+                    if (schoolYearOne[i].kwartaal[j].course[k].exam[l].grade >= 5.5) {
+                        myTotalEC += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
+                    }
+                }
             }
         }
-        }
     }
+    console.log(totalEC);
+    console.log(myTotalEC);
+    return myTotalEC;
 }
-console.log(totalEC);
-console.log(myTotalEC);
-return myTotalEC;
-}
-myTotalEC = calculateMyEc (schoolYearOne);
+myTotalEC = calculateMyEc(schoolYearOne);
 
-function updateProgressBar(array){
+function updateProgressBar(array) {
     for (i = 0; i < schoolYearOne.length; i++) {
         for (j = 0; j < schoolYearOne[i].kwartaal.length; j++) {
             for (k = 0; k < schoolYearOne[i].kwartaal[j].course.length; k++) {
                 for (l = 0; l < schoolYearOne[i].kwartaal[j].course[k].exam.length; l++) {
                     TotaalEc += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
-                if (schoolYearOne[i].kwartaal[j].course[k].exam[l].grade >= 5.5) {
-                    myEc += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
+                    if (schoolYearOne[i].kwartaal[j].course[k].exam[l].grade >= 5.5) {
+                        myEc += schoolYearOne[i].kwartaal[j].course[k].exam[l].weight;
+                    }
                 }
             }
-            }
         }
-    if (myEc == TotaalEc){
-    console.log(`Je hebt kwartaal ${i + 1} 100% voltooid`)
-    //set color to green
+        if (myEc == TotaalEc) {
+            console.log(`Je hebt kwartaal ${i + 1} 100% voltooid`)
+            //set color to green
+        }
+        myEc = 0;
+        TotaalEc = 0;
     }
-    myEc = 0;
-    TotaalEc = 0;
-}
 
-myProgress = (myTotalEC / totalEC) * 100;
-if (myProgress >= 90){
-// set progressbar to myProgress
-    makeProgressBarBlue()   
-}else if (myProgress < 90 && myProgress >= 75){
-    // set progressbar to myProgress
-    makeProgressBarYellow()
-}else {
-    // set progressbar to myProgress
-    makeProgressBarRed()
+    myProgress = (myTotalEC / totalEC) * 100;
+    if (myProgress >= 90) {
+        // set progressbar to myProgress
+        makeProgressBarBlue()
+    } else if (myProgress < 90 && myProgress >= 75) {
+        // set progressbar to myProgress
+        makeProgressBarYellow()
+    } else {
+        // set progressbar to myProgress
+        makeProgressBarRed()
+    }
 }
-}
-updateProgressBar(schoolYearOne);
+//updateProgressBar(schoolYearOne);
 
-function makeTable(){
+function makeTable() {
     //table aanmaken
-table = document.createElement("table");
-src = document.getElementById("newTable");
-table.classList.add("border");
-src.appendChild(table);
-//tr1 aanmaken
-tr1 = document.createElement("tr");
-tr1.appendChild(table);
-//th1 elementen aanmaken
-th1 = document.createElement("th");
-tr1.appendChild(th1);
-//th2 elementen aanmaken
-th2 = document.createElement("th");
-tr1.appendChild(th2);
-//th3 elementen aanmaken
-th3 = document.createElement("th");
-tr1.appendChild(th3);
-//th4 elementen aanmaken
-th4 = document.createElement("th");
-tr1.appendChild(th4);
-//th5 elementen aanmaken
-th5 = document.createElement("th");
-tr1.appendChild(th5);
-//th6 elementen aanmaken
-th6 = document.createElement("th");
-tr1.appendChild(th6);
-for (i = 0; i < 6; i++) {
-    tr2 = document.createElement("tr");
-    table.appendChild(tr2);
-    //td1 elementen aanmaken
-    td1 = document.createElement("th");
+    table = document.createElement("table");
+    src = document.getElementById("newTable");
+    src.appendChild(table);
+    //tr1 aanmaken
+    tr1 = document.createElement("tr");
+    table.appendChild(table);
+    //th1 elementen aanmaken
+    th1 = document.createElement("th").innerHTML = 'Blok';
+    tr1.appendChild(th1);
+    //th2 elementen aanmaken
+    th2 = document.createElement("th").innerHTML = 'Cursus';
+    tr1.appendChild(th2);
+    //th3 elementen aanmaken
+    th3 = document.createElement("th").innerHTML = 'EC';
+    tr1.appendChild(th3);
+    //th4 elementen aanmaken
+    th4 = document.createElement("th").innerHTML = 'Toets';
+    tr1.appendChild(th4);
+    //th5 elementen aanmaken
+    th5 = document.createElement("th").innerHTML = 'Cijfer';
+    tr1.appendChild(th5);
+    //tr2 aanmaken
+    tr2 = document.createElement("tr")
+    tr2.appendChild(table);
+    //th1 elementen aanmaken
+    td1 = document.createElement("td").innerHTML = '1';
+    td1.rowspan = 3
     tr2.appendChild(td1);
     //th2 elementen aanmaken
-    td2 = document.createElement("th");
+    td2 = document.createElement("td").innerHTML = 'Program & carreer orientation';
     tr2.appendChild(td2);
     //th3 elementen aanmaken
-    td3 = document.createElement("th");
+    td3 = document.createElement("td").innerHTML = '2,5';
     tr2.appendChild(td3);
     //th4 elementen aanmaken
-    td4 = document.createElement("th");
+    td4 = document.createElement("td").innerHTML = 'assessment';
     tr2.appendChild(td4);
     //th5 elementen aanmaken
-    td5 = document.createElement("th");
+    td5 = document.createElement("td").innerHTML = '-';
     tr2.appendChild(td5);
-    //th6 elementen aanmaken
-    td6 = document.createElement("th");
-    tr2.appendChild(td6);
 }
-}
+
 makeTable()
